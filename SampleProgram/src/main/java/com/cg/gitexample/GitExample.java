1556e3acb11eb008163bf93cd5403a912b6212b1
Bhagaban Kundu <bhagaban.npt@gmail.com>
2020-03-30 01:23:15 +0530

    Report class updated.

--- SampleProgram/src/main/java/com/cg/gitexample/GitExample.java
+++ SampleProgram/src/main/java/com/cg/gitexample/GitExample.java
@@ -3,7 +3,10 @@ package com.cg.gitexample;
 public class GitExample {
 
 	public static void main(String[] args) {
+		ReportClass rp= new ReportClass();
+		rp.print();
 		System.out.println("GitHub Example");
+	
 
 	}
 
