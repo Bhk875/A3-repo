9d61ccc5d60dfbd8cf7adbad0dae596c4c419bec
Bhagaban Kundu <bhagaban.npt@gmail.com>
2020-03-30 00:17:23 +0530

    user class added

---
+++ SampleProgram/src/main/java/com/cg/gitexample/UserClass.java
@@ -0,0 +1,20 @@
+package com.cg.gitexample;
+
+public class UserClass {
+	public String userId;
+	public String password;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
